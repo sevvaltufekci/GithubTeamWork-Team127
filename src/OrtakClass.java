034e615301d420c04ab1ee717aab029e354e7be9
sevvaltufekci <sevval.kiransoy@gmail.com>
2023-12-14 09:44:29 +0100

    first commit

---
+++ src/OrtakClass.java
@@ -0,0 +1,5 @@
+public class OrtakClass {
+    public static void main(String[] args) {
+        System.out.println("Team calısması");
+    }
+}
---
+++ src/gitHub.java
@@ -0,0 +1,15 @@
+public class gitHub {
+    public static void main(String[] args) {
+        System.out.println("GitHub Col");
+/*
+        git init
+        git add
+        git commit -m "first commit"
+        git branch -M main
+        git remote add origin https://github.com/sevvaltufekci/GithubTeamWork-Team127.git
+        git push -u origin main
+
+
+ */
+    }
+}
